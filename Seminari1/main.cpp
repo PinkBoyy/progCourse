f8aef5168999a8fb7d25926f3581ca8c76175317
PinkBoyy <romeokaraputadze@gmail.com>
2022-01-08 08:55:25 +0400

    updating

--- Seminari1/main.cpp
+++ Seminari1/main.cpp
@@ -15,18 +15,32 @@ double dFactorialCalculator(int);
 //deretmine float/double precisions
 void check();
 
+template <class T>
+T factorialCalculator(int a){
+  T answer = 1;
+  while(a > 0){
+    answer *= a;
+    a--;
+  }
+
+  return answer;
+}
+
 //instructions if input is incorrect
 void helper();
+
+
 
 
 int main(int argc, char** argv) {
   
   //checking input validity
-  if(argc == 4) cout << calculator(argc, argv);
+  if(argc == 4) cout << calculator(argc, argv) << endl;
   
-  else if(argc == 3 && atof(argv[1]) < 22) cout << lFactorialCalculator(atof(argv[1]));
+  //check which type we need { Unsigned Long or Double}
+  else if(argc == 3 && atof(argv[1]) < 22 && *argv[2] == '!') cout << factorialCalculator<unsigned int>(atof(argv[1])) << endl;
 
-  else if(argc == 3 && atof(argv[1]) >= 22) cout << dFactorialCalculator(atof(argv[1]));
+  else if(argc == 3 && atof(argv[1]) >= 22 && *argv[2] == '!') cout << factorialCalculator<double>(atof(argv[1])) << endl;
 
   else helper();
 
@@ -72,8 +86,12 @@ double dFactorialCalculator(int a){
 
 }
 
+
 void helper(){
   cout << "Some instructions for user..\n";
+  cout << "To use just {*, /, +, -}, pattern is like that: { a + b }\n";
+  cout << "To use factorial, pattern is: { a ! }\n";
+  cout << "Where a and b are numbers..\n";
 }
 
 void check(){
